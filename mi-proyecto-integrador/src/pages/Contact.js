7f87f23cb07e6417e13ff0188bdd87141e86264d
chikis0413 <141794603+chikis0413@users.noreply.github.com>
2025-04-23 02:01:05 +0000

    5

--- mi-proyecto-integrador/src/pages/Contact.js
+++ mi-proyecto-integrador/src/pages/Contact.js
@@ -4,7 +4,7 @@ function Contact() {
   return (
     <div style={{ padding: '2rem' }}>
       <h2>Contacto</h2>
-      <p>Puedes contactarme al correo: <strong>miemail@ejemplo.com</strong></p>
+      <p>Puedes contactarme al correo: <strong>sdguillenp@academia.usbbog.edu.co</strong></p>
     </div>
   );
 }
